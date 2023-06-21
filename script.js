f6e39f1601219d82e214de86513a9fa6b5e86e24
Douglas Cavalcante <38563983+douglas-cavalcante@users.noreply.github.com>
2023-06-20 22:04:17 -0300

    adiciona codigo inicial

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function clicarNoBotao() {
+    var email = document.getElementById("campo-email").value
+    var senha = document.getElementById("campo-senha").value
+
+    if(email === "") {
+       alert("Email é obrigatorio")
+    }
+}
+
+
