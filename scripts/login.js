e8300a8bf96ec8b416c522e8296aee1e9a9ff1df
Douglas Cavalcante <38563983+douglas-cavalcante@users.noreply.github.com>
2023-07-03 21:51:27 -0300

    adiciona codigo login

--- scripts/login.js
+++ scripts/login.js
@@ -1,29 +1,52 @@
+const usuarios = [
+    { email: 'henrique@gmail.com', password: 'devinhouse123' },
+    { email: 'douglas@gmail.com', password: 'devinhouse321' },
+    { email: 'cavalcante@gmail.com', password: 'senha' }
+]
 
 function clicarNoBotao() {
-    var email = document.getElementById("campo-email").value
-    var senha = document.getElementById("campo-senha").value
+    const email = document.getElementById("campo-email").value
+    const senha = document.getElementById("campo-senha").value
 
     document.getElementById('campo-email').classList.remove("input-error")
     document.getElementById('campo-senha').classList.remove("input-error")
-    
+
     if (email === "") {
         /* adiciona um estilo inline */
         // document.getElementById('campo-email').style = "border-color: red"
 
-         /* adiciona um estilo direto no objeto */
+        /* adiciona um estilo direto no objeto */
         // document.getElementById('campo-email').style.backgroundColor = 'red'
-        
+
         document.getElementById('campo-email').classList.add("input-error")
         document.getElementById('campo-email').focus()
-    } else if(senha === "") {
+    } else if (senha === "") {
         document.getElementById('campo-senha').classList.add("input-error")
         document.getElementById('campo-email').focus()
     } else {
+     
         document.getElementById('login-button').disabled = true
         document.getElementById('login-button').style.opacity = 0.5
-        document.getElementById('login-button').innerText = "Logando ..." 
-        // redireciona para outra página
-        window.location.href = "./home.html";
+        document.getElementById('login-button').innerText = "Logando ..."
+
+        const usuarioEncontrado = usuarios.find(
+            usuario => usuario.email === email && usuario.password === senha
+        )
+
+        if (usuarioEncontrado) {
+            // redireciona para outra página
+            window.location.href = "./home.html";
+        } else {
+            
+            document.getElementById('login-button').disabled = false
+            document.getElementById('login-button').style.opacity = 1
+            document.getElementById('login-button').innerText = "Entrar"
+
+            alert("Usuário não foi encontrado")
+        }
+
+
+
     }
 }
 
