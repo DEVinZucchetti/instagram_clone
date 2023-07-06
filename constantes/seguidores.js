805d008de78b7bad1697dd42cb115ce1a6c61c13
Douglas Cavalcante <38563983+douglas-cavalcante@users.noreply.github.com>
2023-07-05 22:04:09 -0300

    adiciona pesquisa de seguidores

---
+++ constantes/seguidores.js
@@ -0,0 +1,74 @@
+export const seguidores = [
+    {
+        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
+        nome: 'João Silva',
+        quantidade_seguidores: 1000,
+        quantidade_publicacoes: 50
+    },
+    {
+        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
+        nome: 'Maria Santos',
+        quantidade_seguidores: 500,
+        quantidade_publicacoes: 120
+    },
+    {
+        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
+        nome: 'Pedro Oliveira',
+        quantidade_seguidores: 200,
+        quantidade_publicacoes: 75
+    },
+    {
+        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
+        nome: 'Ana Souza',
+        quantidade_seguidores: 800,
+        quantidade_publicacoes: 100
+    },
+    {
+        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
+        nome: 'Lucas Pereira',
+        quantidade_seguidores: 350,
+        quantidade_publicacoes: 200
+    },
+    {
+        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
+        nome: 'Camila Alves',
+        quantidade_seguidores: 1500,
+        quantidade_publicacoes: 90
+    },
+    {
+        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
+        nome: 'João Silva',
+        quantidade_seguidores: 1000,
+        quantidade_publicacoes: 50
+    },
+    {
+        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
+        nome: 'Maria Santos',
+        quantidade_seguidores: 500,
+        quantidade_publicacoes: 120
+    },
+    {
+        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
+        nome: 'Pedro Oliveira',
+        quantidade_seguidores: 200,
+        quantidade_publicacoes: 75
+    },
+    {
+        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
+        nome: 'Ana Souza',
+        quantidade_seguidores: 800,
+        quantidade_publicacoes: 100
+    },
+    {
+        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
+        nome: 'Lucas Pereira',
+        quantidade_seguidores: 350,
+        quantidade_publicacoes: 200
+    },
+    {
+        url: 'https://img.freepik.com/fotos-gratis/pessoa-de-origem-indiana-se-divertindo_23-2150285283.jpg?w=740&t=st=1688598633~exp=1688599233~hmac=50bfd56c918c046b6a624c9abcff17dd937c453d73afd8d00b98a21ff177e72a',
+        nome: 'Camila Alves',
+        quantidade_seguidores: 1500,
+        quantidade_publicacoes: 90
+    },
+];
