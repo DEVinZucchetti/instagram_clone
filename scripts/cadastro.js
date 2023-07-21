30c3064ca81e5d6686f8a57904178fd23ad4e826
Douglas Cavalcante <38563983+douglas-cavalcante@users.noreply.github.com>
2023-07-20 21:30:38 -0300

    adiciona exemplo com fetch

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,44 +1,84 @@
 import { regexEmail, regexTelefone } from '../utils/regexs.js'
 
+const campoNomeCompleto = document.getElementById('campo-nome-completo')
+const campoSenha = document.getElementById('campo-senha')
+const campoTelefone = document.getElementById('campo-telefone')
+const campoEmail = document.getElementById('campo-email')
+
 document.getElementById('form-cadastro').addEventListener('submit', fazerCadastro)
 
 function fazerCadastro(event) {
 
     event.preventDefault()
 
-    const nome = document.getElementById('campo-nome-completo').value
-    const senha = document.getElementById('campo-senha').value
-    const telefone = document.getElementById('campo-telefone').value
-    const email = document.getElementById('campo-email').value
+    const nome = campoNomeCompleto.value
+    const senha = campoSenha.value
+    const telefone = campoTelefone.value
+    const email = campoEmail.value
 
-    document.getElementById('campo-nome-completo').classList.remove("input-error")
+    campoNomeCompleto.classList.remove("input-error")
     document.getElementById('error-nome').hidden = true
 
-    document.getElementById('campo-senha').classList.remove("input-error")
+    campoSenha.classList.remove("input-error")
     document.getElementById('error-senha').hidden = true
 
-    document.getElementById('campo-telefone').classList.remove("input-error")
+    campoTelefone.classList.remove("input-error")
     document.getElementById('error-telefone').hidden = true
 
-    document.getElementById('campo-email').classList.remove("input-error")
+    campoEmail.classList.remove("input-error")
     document.getElementById('error-email').hidden = true
 
     if (nome === "") {
-        document.getElementById('campo-nome-completo').classList.add('input-error')
-        document.getElementById('campo-nome-completo').focus()
+        campoNomeCompleto.classList.add('input-error')
+        campoNomeCompleto.focus()
         document.getElementById('error-nome').hidden = false
         document.getElementById('error-nome').innerText = "O nome é obrigatório"
     } else if (senha.length < 9) {
-        document.getElementById('campo-senha').classList.add('input-error')
+        campoSenha.classList.add('input-error')
         document.getElementById('error-senha').hidden = false
         document.getElementById('error-senha').innerText = "A senha deve no minimo 8 caracteres"
     } else if (regexTelefone.test(telefone) === false) {
-        document.getElementById('campo-telefone').classList.add('input-error')
+        campoTelefone.classList.add('input-error')
         document.getElementById('error-telefone').hidden = false
         document.getElementById('error-telefone').innerText = "Digite um telefone válido"
     } else if (regexEmail.test(email) === false) {
-        document.getElementById('campo-email').classList.add('input-error')
+        campoEmail.classList.add('input-error')
         document.getElementById('error-email').hidden = false
         document.getElementById('error-email').innerText = "Digite um email válido"
+    } else {
+        // cadastrar os dados que foram digitados no formulario
+
+        /*      
+            GET - lista varias informações 
+            POST - cadastra uma novo recurso
+            DELETE - deletar um recurso
+            PUT - Atualiza todas as informacoes 
+            PATCH - Atualiza uma informação 
+        */
+
+        document.getElementById('login-button').style.opacity = 0.5
+        document.getElementById('login-button').innerText = "Cadastrando ...."
+        document.getElementById('login-button').disabled = true
+    
+        fetch('http://localhost:3333/usuarios', {
+            method: 'POST',
+            body: JSON.stringify({
+                nome: nome,
+                senha: senha,
+                telefone: telefone,
+                email: email
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(() => {
+            alert('Cadastrado com sucesso')
+            window.location.href = "./index.html"
+        })
+        .catch(() => {
+            alert("Desculpe. Houve um erro ao cadastrar o usuario")
+        })
     }
 }
